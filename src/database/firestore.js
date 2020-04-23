b4e0a5f161b78c303d719809f6ba14748f03f7ba
Emil Wojcik <info@emilwojcik.com>
2020-04-23 17:20:13 +0100

    dummy data and add new product firebase connection

--- src/database/firestore.js
+++ src/database/firestore.js
@@ -1,9 +1,9 @@
 
-const products = [];
+export const products = [];
 
 
 function createProducts() {
-    db.collection('products').get().then((snapshot) => {
+    db.collection('test').get().then((snapshot) => {
         snapshot.docs.forEach(doc => {
          products.push(doc.data()) 
         })
@@ -11,4 +11,92 @@ function createProducts() {
 }
 createProducts();
 
-export default products;
+// saving data to firestore 
+
+export const saveProduct = function(name, size, price) {
+    db.collection('test').add({
+        code: 'D0005',
+        product: name,
+        size: size,
+        priceNoVat: price,
+        vat: "0"
+    })
+}
+
+
+
+// dummy data
+// const products = [
+//     {
+//         category:"Drinks - Soft Drinks",
+//         code:"D000001",
+//         product:"Calpis Water (Bottle)",
+//         size:"24 x 500ml",
+//         priceNoVat:"25",
+//         vat:"5",
+//         priceVat:"30",
+//         updated:"08/01/2019"
+//     },
+//     {
+//         category:"Drinks - Soft Drinks",
+//         code:"D000001",
+//         product:"Calpis Water (Bottle)",
+//         size:"24 x 500ml",
+//         priceNoVat:"25",
+//         vat:"5",
+//         priceVat:"30",
+//         updated:"08/01/2019"
+//     },
+//     {
+//         category:"Drinks - Soft Drinks",
+//         code:"D000001",
+//         product:"Calpis Water (Bottle)",
+//         size:"24 x 500ml",
+//         priceNoVat:"25",
+//         vat:"5",
+//         priceVat:"30",
+//         updated:"08/01/2019"
+//     },
+//     {
+//         category:"Drinks - Soft Drinks",
+//         code:"D000001",
+//         product:"Calpis Water (Bottle)",
+//         size:"24 x 500ml",
+//         priceNoVat:"25",
+//         vat:"5",
+//         priceVat:"30",
+//         updated:"08/01/2019"
+//     },
+//     {
+//         category:"Drinks - Soft Drinks",
+//         code:"D000001",
+//         product:"Calpis Water (Bottle)",
+//         size:"24 x 500ml",
+//         priceNoVat:"25",
+//         vat:"5",
+//         priceVat:"30",
+//         updated:"08/01/2019"
+//     },
+//     {
+//         category:"Drinks - Soft Drinks",
+//         code:"D000001",
+//         product:"Calpis Water (Bottle)",
+//         size:"24 x 500ml",
+//         priceNoVat:"25",
+//         vat:"5",
+//         priceVat:"30",
+//         updated:"08/01/2019"
+//     },
+//     {
+//         category:"Drinks - Soft Drinks",
+//         code:"D000001",
+//         product:"Calpis Water (Bottle)",
+//         size:"24 x 500ml",
+//         priceNoVat:"25",
+//         vat:"5",
+//         priceVat:"30",
+//         updated:"08/01/2019"
+//     }
+// ]
+
+
